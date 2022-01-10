aa2595d3dc50bad625f3152108ab17837a2f3887
Fayaz <flalani100@gmail.com>
2022-01-09 23:03:21 -0500

    refactored

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
 const Game = require('./lib/Game');
 
 new Game().initializeGame();
-
-Game.prototype.startNewBattle = function() {
-    if (this.player.agility > this.currentEnemy.agility) {
-      this.isPlayerTurn = true;
-    } else {
-      this.isPlayerTurn = false;
-    }
-
-    console.log('Your stats are as follows:');
-    console.table(this.player.getStats());
-    console.log(this.currentEnemy.getDescription());
-
-    this.battle()
-
-  };
